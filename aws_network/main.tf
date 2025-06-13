b9bb213df6d756fb1805404c84a136ffbd4973d0
Saima Anam Syed <101432736+saimaanam@users.noreply.github.com>
2025-06-13 08:43:52 -0600

    Update main.tf with igw and RT

--- aws_network/main.tf
+++ aws_network/main.tf
@@ -5,6 +5,14 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.main.id
@@ -14,4 +22,23 @@ resource "aws_subnet" "public" {
   tags = {
     Name = "${var.environment}-public-subnet-${count.index + 1}"
   }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
